b6a1d98f199961c671c8a693bba70cf2df7441e0
uctan <duytan20032015@gmail.com>
2024-09-10 14:38:46 +0700

    update vlast

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {QueryClientProvider,QueryClient} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const queryClient = new QueryClient()
-root.render(
-  // <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -20,8 +19,8 @@ root.render(
       </PersistGate>
     </Provider>
     <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  // </React.StrictMode>
+  </QueryClientProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
